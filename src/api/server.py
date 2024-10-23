2b0db0004827322c95482ee2cf815cd43ec35c57
Joe Simopoulos <joesimop8@gmail.com>
2024-10-22 18:23:16 -0700

    add coors

--- src/api/server.py
+++ src/api/server.py
@@ -22,6 +22,16 @@ app = FastAPI(
     },
 )
 
+origins = ["http://localhost", "http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(market_manager.router)
 app.include_router(vendor.router)
 app.include_router(checkout.router)
